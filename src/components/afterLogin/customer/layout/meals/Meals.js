fe55073a873b333e8658079012ec944ab810c5e0
Saransh Kapoor <123106127+Saransh05@users.noreply.github.com>
2024-09-20 23:26:13 +0530

    Delete src directory

--- src/components/afterLogin/customer/layout/meals/Meals.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AvailableMeals from "./AvailableMeals";
-
-const Meals = (props) => {
-  return <AvailableMeals dishes={props.dishes} />;
-};
-
-export default Meals;
